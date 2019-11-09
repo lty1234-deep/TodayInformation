efadba14392bc8d7f98a4619c479a0503f16db65
lty <15670097092@163.com>
2019-11-09 10:41:32 +0800

    动画和MVP设计模式

--- app/src/main/java/com/android/todayinformation/SplashActivity.java
+++ app/src/main/java/com/android/todayinformation/SplashActivity.java
@@ -8,63 +8,103 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.VideoView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.todayinformation.mvp.ISplashActivityContract;
 
 import java.io.File;
 
-public class SplashActivity extends AppCompatActivity {
+import butterknife.BindView;
 
-    private VideoView mVideoView;
-    private TextView mTvTimer;
-    private CustomCountDownTimer timer;
+@ViewInject(mainlayoutid = R.layout.activity_splash)
+public class SplashActivity extends BaseAcivity implements ISplashActivityContract.Iview {
+
+    @BindView(R.id.vv_play)
+    FullScreenVideoView mVideoView;
+    @BindView(R.id.tv_splash_time)
+    TextView mTvTimer;
+    private ISplashActivityContract.IPresenter timerPresenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+//        setContentView(R.layout.activity_splash);
 
-        mVideoView = findViewById(R.id.vv_play);
-        mTvTimer = findViewById(R.id.tv_splash_time);
-        mTvTimer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
 
-            }
-        });
-        mVideoView.setVideoURI(Uri.parse("android.resource://"+ getPackageName()+ File.separator+R.raw.splash));
+//        initTimer();
+
+    }
+
+    @Override
+    public void afterBindView() {
+        initTimerPresenter();
+
+        initListener();
+        initVideo();
+    }
+
+    private void initTimerPresenter() {
+        timerPresenter = new SplashTimerPresenter(this);
+        timerPresenter.initTimer();
+    }
+
+//    private void initTimer() {
+//
+//        timer = new CustomCountDownTimer(5, new CustomCountDownTimer.ICountDownHandler() {
+//            @Override
+//            public void onTicker(int time) {
+//                mTvTimer.setText(time + "秒");
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//                mTvTimer.setText("跳过");
+//
+//
+//            }
+//        });
+//        timer.start();
+//    }
+
+    private void initVideo() {
+        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + File.separator + R.raw.splash));
         mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 mp.start();
             }
         });
+
+    }
+
+    private void initListener() {
+
+        mTvTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
         mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 mp.start();
             }
         });
-        timer = new CustomCountDownTimer(5, new CustomCountDownTimer.ICountDownHandler() {
-            @Override
-            public void onTicker(int time) {
-                mTvTimer.setText(time+"秒");
-            }
-
-            @Override
-            public void onFinish() {
-                mTvTimer.setText("跳过");
 
 
-            }
-        });
-        timer.start();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        timer.cancel();
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        timerPresenter.onDestroy();
+//    }
+
+    public void setTvTimer(String s) {
+        mTvTimer.setText(s);
     }
 }
